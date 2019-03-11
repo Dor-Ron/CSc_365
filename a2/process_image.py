e1016701fc89e7dfc9caf7ed1e18976789dbeae5
Dor-Ron <dor.rondel@gmail.com>
2019-03-11 03:33:27 -0400

    started opencv gruntwork

--- a2/process_image.py
+++ a2/process_image.py
@@ -1,5 +1,8 @@
 __author__ = "Dor Rondel"
 
+import cv2
+import numpy as np
+
 # step one - read image
 filename = ""
 while True:
@@ -10,12 +13,19 @@ active running directory:
 **************************************
 """))
 
-    if filename[filename.index(".") + 1:] in ["jpg", "png", "bmp", "jpeg"]:
+    if filename[filename.index(".") + 1:] in ["jpg", "png", "bmp", "jpeg", "tiff"]:
+        # opencv doesnt support gif unfortunately
         break
 
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+for i in range(100):
+    print(thresh[i, i])
 
+cv2.imshow('image',thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # step two - convert image to greyscale
-# step three - binarize image
-# step four - read and lable components
-# step five - render new image with components
+# step three - read and lable components
+# step four - render new image with components
