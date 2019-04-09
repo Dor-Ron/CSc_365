c08686d731026ef5a352fd14023b8975427b9ece
Dor-Ron <dor.rondel@gmail.com>
2019-04-08 21:40:58 -0400

    added btree project boilerplate

---
+++ a3/src/main/java/oswego/csc365/a3/Main.java
@@ -0,0 +1,13 @@
+package oswego.csc365.a3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ a3/src/test/java/oswego/csc365/a3/MainTest.java
@@ -0,0 +1,20 @@
+package oswego.csc365.a3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
