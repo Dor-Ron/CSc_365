9306bba33e83caa284c9b0721448beb734ea001b
Dor-Ron <dor.rondel@gmail.com>
2019-04-29 02:13:38 -0400

    started implementation to final assignment

---
+++ a5/Adjacency_List.py
@@ -0,0 +1,6 @@
+class Adjacency_List(object):
+    ''' Class to create adjacency lists from files of specified format '''
+
+    def __init__(self, filepath):
+        ''' constructor for adjacency list class '''
+        pass
---
+++ a5/__init__.py
@@ -0,0 +1,5 @@
+from Adjacency_List import Adjacency_List as Graph
+from Profit_Flow_Network import Profit_Flow_Network as Network
+
+if __name__ == "__main__":
+    pass
