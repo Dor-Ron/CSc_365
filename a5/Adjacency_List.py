34862b9a6c499b3ad7a6addfd6717715373f5494
Dor-Ron <dor.rondel@gmail.com>
2019-04-29 22:12:54 -0400

    removed old unused files

--- a5/Adjacency_List.py
+++
@@ -1,6 +0,0 @@
-class Adjacency_List(object):
-    ''' Class to create adjacency lists from files of specified format '''
-
-    def __init__(self, filepath):
-        ''' constructor for adjacency list class '''
-        pass
